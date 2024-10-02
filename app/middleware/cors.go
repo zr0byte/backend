646860c6d29b759ff36d543555938ab0673f31e8
nitin <nitinmewar28@gmail.com>
2024-10-02 12:09:33 +0530

    Update cors.go

--- app/middleware/cors.go
+++ app/middleware/cors.go
@@ -8,12 +8,13 @@ import (
 
 func CORSMiddleware() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
-		ctx.Writer.Header().Set("Acess-Control-Allow-Origin", "*")
+		ctx.Writer.Header().Set("Access-Control-Allow-Origin", "*")
 		ctx.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
-		ctx.Writer.Header().Set("Access-Control-Allow-Headers", "Content-type, Content-length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-control, X-Requested-With")
+		ctx.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
 		ctx.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
 
 		if ctx.Request.Method == http.MethodOptions {
+			// If it's an OPTIONS request, just return with 204 status
 			ctx.AbortWithStatus(http.StatusNoContent)
 			return
 		}
@@ -21,3 +22,4 @@ func CORSMiddleware() gin.HandlerFunc {
 		ctx.Next()
 	}
 }
+
